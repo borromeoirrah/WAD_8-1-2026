3c9709b66881e4d1489e4f1be3ae8c3b0e2e61ec
borromeoirrah <borromeoirrah@gmail.com>
2026-08-01 07:40:09 +0800

    add score var

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 let score = 0;
+
+const add = document.querySelector("#addnum");
+const subtract = document.querySelector("#subtractnum");
+const reset = document.querySelector("#resetnum");
+const display = document.querySelector("#Displaynum");
+
+
+add.addEventListener("click", () => {
+    score++;
+    display.textContent = `Score: ${score}`;
+}
+);
+
+subtract.addEventListener("click", () => {
+    score--;
+    display.textContent = `Score: ${score}`;
+}
+);       
+
+reset.addEventListener("click", () => {
+    score = 0;
+    display.textContent = `Score: ${score}`;
+}
+);
